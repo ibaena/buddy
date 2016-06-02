3f2ad89bbf0a820ea829993ad92972248eeeb1d7
Ivan <ivan.baena04@gmail.com>
2016-06-02 16:30:48 -0400

    search bar list functioning; makiplementing call for overall search

--- client/buddy/SearchBar.jsx
+++ client/buddy/SearchBar.jsx
@@ -8,7 +8,7 @@ export default class SearchBar extends TrackerReact(Component) {
   // Note: In ES6, constructor() === componentWillMount() in React ES5
   constructor() {
     super();
-
+     this.state = { breedList: [] };
   }
 
 
@@ -17,87 +17,81 @@ export default class SearchBar extends TrackerReact(Component) {
  componentDidMount(){
     $('select').material_select();
 
-    $('#animal-selection').change(function() {
-      let animalType = $(this).val();
+    Meteor.call("breedList",  {query: 'dog'}, (error, res) => {
+     if(error) {
+       console.log("error:", error);
 
-        Meteor.call("breedList",  {query: animalType}, (error, res) => {
-         if(error) {
-           console.log("error:", error);
+     } else {
+        this.setState({breedList: res.data.petfinder.breeds.breed});
+     }
+   });
 
-         } else {
-           console.log(res);
-         }
-       });
+ }
 
-    });
+ submitForm(){
+
+
  }
 
 
 
+
   render() {
+
     return (
     <div className="row">
       <form className="col s12">
         <div className="input-field col s2">
-        <input id="zip" type="search" required />
-        <label for="zip">Enter Your Zip Code</label>
-        </div>
-
-        <div className="input-field col s2" id="animal">
-          <select id="animal-selection">
-            <option value="dog">Dog</option>
-            <option value="cat">Cat</option>
-            <option value="bird">Bird</option>
-            <option value="reptile">Reptile</option>
-            <option value="horse">Horse</option>
-            <option value="pig">Pig</option>
-            <option value="barnyard">Farm</option>
-          </select>
-          <label>Choose an Animal</label>
+        <input id="zip" type="search" placeholder="Zip" required />
         </div>
 
         <div className="input-field col s2" id="breed">
-          <select>
-            <option value="0">Pit Bull Terrier</option>
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
+          <select className="browser-default">
+
+            {
+              this.state.breedList.map((breed) => {
+                return (
+
+                  <option key={breed.$t} value={breed.$t}>{breed.$t}</option>
+              )
+            })
+          }
           </select>
-          <label>Choose a Breed</label>
         </div>
 
         <div className="input-field col s2" id="size">
-          <select>
+          <select className="browser-default">
             <option value="S">Small</option>
             <option value="M">Medium</option>
             <option value="L">Large</option>
             <option value="XL">Extra large</option>
           </select>
-          <label>Choose a Size</label>
         </div>
 
         <div className="input-field col s2" id="age">
-          <select>
+          <select className="browser-default">
             <option value="Baby">Baby</option>
             <option value="Young">Young</option>
             <option value="Adult">Adult</option>
             <option value="Senior">Senior</option>
           </select>
-          <label>Choose an Age</label>
         </div>
 
         <div className="input-field col s2" id="gender">
-          <select>
+          <select className="browser-default">
             <option value="M">Male</option>
             <option value="F">Female</option>
           </select>
-          <label>Choose a Gender</label>
+        </div>
+
+        <div className="input-field col s2" id="gender">
+          <button className="btn waves-effect waves-light black" type="submit" name="action">Submit
+            <i className="material-icons right">send</i>
+          </button>
         </div>
 
       </form>
-      <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-        <i className="material-icons right">send</i>
-      </button>
+
     </div>
     )
   }
